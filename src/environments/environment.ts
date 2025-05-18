d1e1eaa370fb11c9e3cbd11a2125fa305ec9bf36
Tőzsér Imre Márk <h270349@stud.u-szeged.hu>
2025-05-18 20:28:27 +0200

    aa

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB0XJ0Ow2Bce5V_JwjzNQk0InNHunfha-c",
+  authDomain: "eskuvoi-helyszinfoglalo.firebaseapp.com",
+  projectId: "eskuvoi-helyszinfoglalo",
+  storageBucket: "eskuvoi-helyszinfoglalo.firebasestorage.app",
+  messagingSenderId: "578737961234",
+  appId: "1:578737961234:web:686cbeab4dcadd34cadbcb"
+  }
+};
